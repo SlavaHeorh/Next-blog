dfe51d52d3714f94fc929fa4c7d3513b42ca0dbc
SlavaHeorh <slava.georg.k@gmail.com>
2023-04-28 12:59:49 +0300

    added posts and post pages

---
+++ pages/blog/[id].tsx
@@ -0,0 +1,27 @@
+import Layout from "../../components/Layout";
+
+const SinglePost = ({post}) => {
+    if (!post)
+        return <p>"Post Doesn't exist"</p>
+
+    return (
+        <Layout title={post.title} description={post.body}>
+            <h1>{post.title}</h1>
+            <p>
+                {post.body}
+            </p>
+        </Layout>
+    )
+}
+
+export const getServerSideProps = async ({query}) => {
+
+    const data = await fetch(`${process.env.API_URL}/posts/${query.id}`)
+    const post = await data.json()
+
+    return {
+        props: {post},
+    }
+}
+
+export default SinglePost;
